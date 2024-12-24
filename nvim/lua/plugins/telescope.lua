5d2b41f272cee8789fac3ad7da0371c079206107
James Zhao <hjz@hackjumpzero.ca>
2024-12-24 15:18:47 -0500

    Use oil.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -50,19 +50,21 @@ return {
         },
         pickers = {
           find_files = {
-            find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+            find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--hidden", "--follow", "--exclude", ".git" },
           },
         },
         extensions = {
           fzf = {
             fuzzy = true,                   -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
+            iverride_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true,    -- override the file sorter
             case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
           }
         }
       })
+
+      require('telescope').load_extension('fzf')
     end
   },
   {
