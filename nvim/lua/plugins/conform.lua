9f20f643756fedae9e75d2a8ca81a57672ef11ae
James Zhao <hjz@hackjumpzero.ca>
2024-12-24 11:07:16 -0500

    Fix some formatter setup

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
     {
       "<leader>f",
       function()
-        require("conform").format({ formatters = { "injected" }, lsp_fallback = true, timeout_ms = 1000, })
+        require("conform").format({ lsp_fallback = true, timeout_ms = 1000, })
       end,
       mode = { "n", "v" },
       desc = "Format Injected Langs",
@@ -28,10 +28,21 @@ return {
       black = {
         prepend_args = { "--fast" },
       },
+      -- gn = {
+      --   prepend_args = { "format", "--stdin" }
+      -- },
+      shfmt = {
+        prepend_args = { "-i", "2" }
+      }
     },
     format_on_save = {
       timeout_ms = 1000,
       lsp_format = "fallback",
     },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    quiet = false,
+    notify_on_error = true,
   },
 }
