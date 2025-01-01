0dd57237605e9bffd22869eb4f89535aecf43b04
James Zhao <hjz@hackjumpzero.ca>
2025-01-01 09:57:39 -0500

    update hop keymap

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -5,9 +5,23 @@ return {
     require('hop').setup()
     vim.keymap.set('n', 'f',
       function()
-        require 'hop'.hint_words({})
+        require 'hop'.hint_char1({
+          direction = require 'hop.hint'.HintDirection.AFTER_CURSOR,
+          current_line_only = true
+        })
       end,
       {})
+
+    vim.keymap.set('n', 'F',
+      function()
+        require 'hop'.hint_char1({
+          direction = require 'hop.hint'.HintDirection.BEFORE_CURSOR,
+          current_line_only = true
+        })
+      end,
+      {})
+
+    vim.keymap.set('n', '<Leader><space>', "<cmd>:HopChar1<cr>", {})
   end
 
 }
