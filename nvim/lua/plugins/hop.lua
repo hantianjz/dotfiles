d4ce165474e67c5f2f1d0f9dc57b86e7210469c1
James Zhao <james@tryfi.com>
2025-01-06 10:41:05 -0500

    Update hop shortcut

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -3,25 +3,7 @@ return {
   enables = false,
   config = function()
     require('hop').setup()
-    vim.keymap.set('n', 'f',
-      function()
-        require 'hop'.hint_char1({
-          direction = require 'hop.hint'.HintDirection.AFTER_CURSOR,
-          current_line_only = true
-        })
-      end,
-      {})
-
-    vim.keymap.set('n', 'F',
-      function()
-        require 'hop'.hint_char1({
-          direction = require 'hop.hint'.HintDirection.BEFORE_CURSOR,
-          current_line_only = true
-        })
-      end,
-      {})
-
-    vim.keymap.set('n', '<Leader><space>', "<cmd>:HopChar1<cr>", {})
+    vim.keymap.set('n', '<Leader><space>', "<cmd>:HopWord<cr>", {})
   end
 
 }
