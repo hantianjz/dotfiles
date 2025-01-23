8962f7eab13f539a3886bf064b4088c49f09b7d5
James Zhao <james@tryfi.com>
2025-01-23 10:50:58 -0500

    clean up unused scripts

--- scripts/display_cycle.sh
+++
@@ -1,8 +0,0 @@
-curr_d_id=$(yabai -m query --displays --space | jq '.id')
-last_d_id=$(yabai -m query --displays | jq '.[-1].id')
-
-if [[ $last_d_id == $curr_d_id ]]; then
-	yabai -m display --focus first
-else
-	yabai -m display --focus next
-fi
