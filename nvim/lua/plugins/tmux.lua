446f28d361bdd552479efdca58be249ce5b7fe7f
James Zhao <hjz@block.xyz>
2025-03-16 20:25:02 -0400

    fix: Update Tmux navigation commands to remove unnecessary colons

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -10,28 +10,28 @@ return {
   keys = {
     {
       "<C-h>",
-      function() vim.cmd([[:TmuxNavigateLeft]]) end,
+      function() vim.cmd([[TmuxNavigateLeft]]) end,
       mode = { "n", "i", "t" },
       silent = true,
       desc = "Navigate to left pane"
     },
     {
       "<C-j>",
-      function() vim.cmd([[:TmuxNavigateDown]]) end,
+      function() vim.cmd([[TmuxNavigateDown]]) end,
       mode = { "n", "i", "t" },
       silent = true,
       desc = "Navigate to bottom pane"
     },
     {
       "<C-k>",
-      function() vim.cmd([[:TmuxNavigateUp]]) end,
+      function() vim.cmd([[TmuxNavigateUp]]) end,
       mode = { "n", "i", "t" },
       silent = true,
       desc = "Navigate to top pane"
     },
     {
       "<C-l>",
-      function() vim.cmd([[:TmuxNavigateRight]]) end,
+      function() vim.cmd([[TmuxNavigateRight]]) end,
       mode = { "n", "i", "t" },
       silent = true,
       desc = "Navigate to right pane"
