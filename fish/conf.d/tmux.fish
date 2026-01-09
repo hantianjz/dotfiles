cbed46f2c78731056401b52814b8a3a7495ad178
James Zhao <hjz@block.xyz>
2026-01-08 22:01:19 -0500

    some update

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -3,16 +3,12 @@ function tmux-from-session-name
   if test -n "$HERMIT_ENV"
       type -q _hermit_deactivate; and _hermit_deactivate
   end
-  tmux new-session -As $argv[1]
+  tmx open $argv[1]
 end
 
 function new-tmux-from-dir-name
   tmux-from-session-name (basename $PWD)
 end
-#
-# function __tmux-list-sessions
-#   tmux list-sessions -F "#{session_name}" 2> /dev/null
-# end
 
 alias t 'tmx'
 alias tmn "new-tmux-from-dir-name"
