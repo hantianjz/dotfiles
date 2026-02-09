df0d0c6d52040f7454d3ee594641bfb582b8a32b
James Zhao <hjz@block.xyz>
2026-02-09 08:47:29 -0500

    revert fzf-lua session resume

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,37 +1,21 @@
-local state = { current = nil, hidden = nil }
-
-local function resumable(name, fn)
-  return function(opts)
-    if state.hidden == name then
-      state.hidden = nil
-      state.current = name
-      require("fzf-lua").resume()
-    else
-      state.current = name
-      state.hidden = nil
-      fn(opts)
-    end
-  end
-end
-
 return {
   {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-      { "<leader>t",  resumable("files", function() require("fzf-lua").files() end),                 desc = "Find files" },
-      { "<leader>g",  resumable("git_files", function() require("fzf-lua").git_files() end),             desc = "Git files" },
-      { "<leader>b",  resumable("buffers", function() require("fzf-lua").buffers() end),               desc = "Current buffers" },
-      { "<leader>vh", resumable("help_tags", function() require("fzf-lua").help_tags() end),             desc = "Vim help tags" },
-      { "<leader>d",  resumable("diagnostics_doc", function() require("fzf-lua").diagnostics_document() end),  desc = "Buffer diagnostics" },
-      { "<leader>D",  resumable("diagnostics_ws", function() require("fzf-lua").diagnostics_workspace() end), desc = "Workspace diagnostics" },
-      { "<leader>lf", resumable("lsp_refs", function() require("fzf-lua").lsp_references() end),        desc = "LSP references" },
-      { "<leader>ld", resumable("lsp_defs", function() require("fzf-lua").lsp_definitions() end),       desc = "LSP definitions" },
-      { "<leader>o",  resumable("builtin", function() require("fzf-lua").builtin() end),               desc = "fzf-lua builtin" },
-      { "<C-q>",      resumable("quickfix", function() require("fzf-lua").quickfix() end),              desc = "Quickfix list" },
-      { "<leader>vc", resumable("config_files", function() require("fzf-lua").files({ cwd = vim.fn.stdpath("config") }) end), desc = "Find nvim config" },
-      { "<leader>vp", resumable("plugin_files", function() require("fzf-lua").files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }) end), desc = "Find nvim plugins" },
-      { "<leader>s",  resumable("multigrep", function() require("config.fzf.live_multi_grep").live_multigrep() end), desc = "Multi-pattern grep" },
+      { "<leader>t",  function() require("fzf-lua").files() end,                                                          desc = "Find files" },
+      { "<leader>g",  function() require("fzf-lua").git_files() end,                                                      desc = "Git files" },
+      { "<leader>b",  function() require("fzf-lua").buffers() end,                                                        desc = "Current buffers" },
+      { "<leader>vh", function() require("fzf-lua").help_tags() end,                                                      desc = "Vim help tags" },
+      { "<leader>d",  function() require("fzf-lua").diagnostics_document() end,                                           desc = "Buffer diagnostics" },
+      { "<leader>D",  function() require("fzf-lua").diagnostics_workspace() end,                                          desc = "Workspace diagnostics" },
+      { "<leader>lf", function() require("fzf-lua").lsp_references() end,                                                 desc = "LSP references" },
+      { "<leader>ld", function() require("fzf-lua").lsp_definitions() end,                                                desc = "LSP definitions" },
+      { "<leader>o",  function() require("fzf-lua").builtin() end,                                                        desc = "fzf-lua builtin" },
+      { "<C-q>",      function() require("fzf-lua").quickfix() end,                                                       desc = "Quickfix list" },
+      { "<leader>vc", function() require("fzf-lua").files({ cwd = vim.fn.stdpath("config") }) end,                        desc = "Find nvim config" },
+      { "<leader>vp", function() require("fzf-lua").files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }) end, desc = "Find nvim plugins" },
+      { "<leader>s",  function() require("config.fzf.live_multi_grep").live_multigrep() end,                              desc = "Multi-pattern grep" },
     },
     config = function()
       local actions = require("fzf-lua.actions")
@@ -55,11 +39,6 @@ return {
                   if config.relative and config.relative ~= "" then
                     return
                   end
-                  -- Only update state if <Esc> hasn't already handled it
-                  if state.current then
-                    state.hidden = state.current
-                    state.current = nil
-                  end
                   if vim.api.nvim_win_is_valid(win) then
                     require("fzf-lua").hide()
                   end
@@ -73,13 +52,7 @@ return {
           },
         },
         keymap = {
-          builtin = {
-            ["<Esc>"] = function(fzf_win)
-              state.hidden = state.current
-              state.current = nil
-              fzf_win:hide()
-            end,
-          },
+          builtin = { ["<Esc>"] = "hide" },
           fzf = { ["ctrl-q"] = "select-all+accept" },
         },
         actions = {
