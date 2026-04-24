ad9be16b612f7b365eb35af7b04b4e9043498d2a
H. James Zhao <hjz@block.xyz>
2026-04-24 09:48:17 -0400

    disable semhl for octo buffer

--- nvim/lua/plugins/semhl.lua
+++ nvim/lua/plugins/semhl.lua
@@ -9,5 +9,15 @@ return {
     filetypes = { "c", "cpp", "h", "python", "lua", "typescript", "java", "rust" },
     max_file_size = 100 * 1024,
     target_delta_e = 30,
+    disable = function(buffer)
+      if vim.api.nvim_buf_get_name(buffer):match("^octo://") then
+        return true
+      end
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buffer))
+      if ok and stats and stats.size > 100 * 1024 then
+        return true
+      end
+      return false
+    end,
   },
 }
